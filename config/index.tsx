32c0b22623278c65584cbc1f2a403b5284dcc352
Kirill <kiria12312@mail.ru>
2024-10-22 02:51:21 +0300

    feat: add config files

---
+++ config/index.tsx
@@ -0,0 +1,5 @@
+export const APPLICATION_ID = '3IU3jpoS2YxqFr8wt4oYRDu1jKaFYYA1J1M2RZLX';
+export const JAVASCRIPT_KEY = '1Dqv2dqEXx3gBZKwK1u0OMthHtx2pVm69W5ZomJF';
+export const SERVER_URL = 'https://parseapi.back4app.com/';
+
+export const API_KEY = '31f21f04-bd0c-4829-bf4c-0902dae35cbb';
